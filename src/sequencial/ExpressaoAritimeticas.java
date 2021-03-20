733337724f3dd69d86af15dcd17aa6caeeb2d63c
italoevil <italoevil1987@gmail.com>
2021-03-20 16:44:19 -0300

    ExpressaoAritimeticas

---
+++ src/sequencial/ExpressaoAritimeticas.java
@@ -0,0 +1,26 @@
+package sequencial;
+
+public class ExpressaoAritimeticas {
+
+	public static void main(String[] args) {
+
+		//ExpressaoAritimeticas  + - * / %
+		
+		// + adição
+		int numero = 0;
+		numero =  numero + 10;
+		
+		// + subtração
+		numero = numero - 3;
+		
+		// * multiplicação
+		numero = numero * 2;
+		
+		// * divisão
+		numero = numero / 2;
+		
+		// % resto da divisão
+		numero = numero % 2; 
+		System.out.println(numero);
+	}
+}
