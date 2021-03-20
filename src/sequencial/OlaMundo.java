8385be574bd0a23dde3fd5d4302860cd86c8631b
italoevil <italoevil1987@gmail.com>
2021-03-20 10:00:42 -0300

    Sequencia logica do programa ENTRADA-PROCESSO-SAIDA

--- src/sequencial/OlaMundo.java
+++ src/sequencial/OlaMundo.java
@@ -1,9 +1,30 @@
 package sequencial;
 
+import java.util.Scanner;
+
 public class OlaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ola Mundo!");
+		//algoritimo exibir nome e email
+		
+		//SEQUENCIAL
+		Scanner sc = new Scanner(System.in);
+		
+		//ENTRADA
+		System.out.print("Informe seu nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Infome seu email: ");
+		String email = sc.nextLine();
+		
+		//PROCESSAMENTO
+		String result = nome + " - " + email;
+		
+		//SAIDA
+		System.out.println(result);
+		
+		
+		sc.close();
+		
 	}
 }
