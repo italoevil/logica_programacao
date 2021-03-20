daf2ef90344229133351a3e5bbf39d765cefdf09
italoevil <italoevil1987@gmail.com>
2021-03-20 17:08:16 -0300

    Variaveis

---
+++ src/sequencial/VariaveisTiposDados.java
@@ -0,0 +1,24 @@
+package sequencial;
+
+public class VariaveisTiposDados {
+
+	public static void main(String[] args) {
+
+		//VARIAVEIS
+		int idade = 15; //ideda de 15 anos, sendo guardado na variavel, para usar quando quiser
+		
+		//EXIBIR A IDADE
+		System.out.println(idade);
+		
+		//CONCATENANDO A IDADE COM STRING
+		
+		System.out.println(idade + " Anos");
+		
+		//SOMANDO MAIS UM ANO Á IDADE
+		
+		int novaIdade = idade + 1;
+		System.out.println(novaIdade); 
+		
+	}
+	
+}
