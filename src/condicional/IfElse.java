83a44c643ebd7b4ec13c67132edf723ae5db845f
italoevil <italoevil1987@gmail.com>
2021-03-20 22:22:36 -0300

    condicional IF ELSE

---
+++ src/condicional/IfElse.java
@@ -0,0 +1,16 @@
+package condicional;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		//IF ELSE
+		int idade = 18;
+		
+		//se
+		if (idade >= 18) {
+			System.out.println("Adulto");
+		}else {
+			System.out.println("Criança");
+		}
+	}
+}
