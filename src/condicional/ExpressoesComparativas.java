b68b915e1ea69d4e42ba33b1bf175c3d5055e1a3
italoevil <italoevil1987@gmail.com>
2021-03-20 22:13:36 -0300

    condicional comparativas

---
+++ src/condicional/ExpressoesComparativas.java
@@ -0,0 +1,23 @@
+package condicional;
+
+public class ExpressoesComparativas {
+
+	public static void main(String[] args) {
+		
+		//ExpressoesComparativas < <= == > >= !=
+		
+		int numero = 10;
+		
+		//< menor
+		System.out.println(numero < 5);
+		//<= menor igual
+		System.out.println(numero <= 5);
+		//> maior
+		System.out.println(numero > 5);
+		//>= maior igual
+		System.out.println(numero >= 5);
+		//!= diferente
+		System.out.println(numero != 5);
+		
+	}
+}
