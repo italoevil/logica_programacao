5087346a8cd5977d1d995f2ea721981bb4ad79f6
italoevil <italoevil1987@gmail.com>
2021-03-20 22:05:33 -0300

    TiposDados

---
+++ src/sequencial/TiposDados.java
@@ -0,0 +1,23 @@
+package sequencial;
+
+public class TiposDados {
+
+	public static void main(String[] args) {
+		
+		//TIPOS DE DADOS
+		/*
+		 * int,double,short,byte,boolean,long,char,void,float
+		 */
+		byte by = 127; 				
+		short sh = 32767;  			
+		int in = 2_147_483_647;
+		long lon = 922_330_000_000l; 
+		
+		double dou = 21.56;
+		float flo = 21.56f;
+		
+		char ch = 'A';
+		
+		boolean boo = false;
+	}
+}
