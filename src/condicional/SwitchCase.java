fa95088b3abb3d47644ae1542283b47d7de40731
italoevil <italoevil1987@gmail.com>
2021-03-20 22:26:25 -0300

    condicional switch

---
+++ src/condicional/SwitchCase.java
@@ -0,0 +1,27 @@
+package condicional;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+
+		//SwitchCase
+		
+		int numero = 5;
+		
+		switch(numero) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 5:
+			System.out.println("5");
+			break;
+		case 10:
+			System.out.println("10");
+			break;
+		default:
+			System.out.println("Sem retorno!");
+		}
+		
+	}
+
+}
